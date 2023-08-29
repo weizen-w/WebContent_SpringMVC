0b53439698dbc7d9cda53fe62310e92738324e4a
Wladimir Weizen <weizen.w@gmx.de>
2023-08-29 14:39:54 +0200

    initial commit

---
+++ src/test/java/com/example/webcontent_springmvc/WebContentSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webcontent_springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebContentSpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
